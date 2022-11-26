e14e82b0e91f584b56b74f9847460ac329eb58c1
Mohd Aasim Qureshi <aasimqureshi31jan@gmail.com>
2022-11-26 15:19:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Welome to my BLOG Website. Here you can see other people's published Blogs, You can publish your own blog by appending '/compose' in the URL and you will be redirected to Compose Page where you can write Title and Content of your Blog and can Publish it.";
+const aboutContent = "Hi! My Self Mohd Aasim, I am currently a final year Undergrad Student at Doon Institute of Engineering and Technology, Rishikesh. I am Specialist at Codeforces, 4 Star on CodeChef, have done 280+ Problems on LeetCode.";
+const contactContent = "Email - aasimqureshi31jan@gmail.com | Phone - +91 - 7037862584";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
